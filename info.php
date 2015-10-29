d08218b00a1647a5ce1b31809ccd9ed9bc26d5bc
johnson <johnson@johnson-VirtualBox>
2015-10-29 13:02:11 +1300

    add info file

---
+++ info.php
@@ -0,0 +1,14 @@
+<?php
+
+$a = 'test';
+$b = 123;
+
+echo $a;
+echo '</br>';
+phpinfo();
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
